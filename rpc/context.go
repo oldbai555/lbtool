c377dad4b91c06d520986236227001803482afb0
baigege <1005777562@qq.com>
2022-08-27 23:43:25 +0800

    ok

---
+++ rpc/context.go
@@ -0,0 +1,21 @@
+package rpc
+
+type ServerCmdList []ServerCmd
+
+type IFunc func(ctx *Context, req *IRequest) (*IResponse, error)
+
+type ServerCmd struct {
+	ServerName string      `json:"serverName"`
+	IFun       interface{} `json:"i_fun"`
+}
+
+type Context struct {
+}
+
+type IRequest interface {
+	GetHeader(name string) (string, error)
+}
+
+type IResponse interface {
+	SetHeader(name string, value string) error
+}
