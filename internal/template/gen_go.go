692ac935fec76711c73b30bfdfadf7aa3206cce6
zhangjianjun <zhangjianjun@aquanliang.com>
2022-08-29 13:43:45 +0800

    gen_template

--- internal/template/gen_go.go
+++ internal/template/gen_go.go
@@ -30,3 +30,37 @@ func GetOsFile(filePath string, fileName string) (file *os.File, err error) {
 	}
 	return
 }
+
+// // GenTemplate 生成模板文件 相对路径
+// func GenTemplate(f *Function) (err error) {
+//
+//	file, err := GetOsFile("./internal/gen_code_file", "gen_init_mq_template")
+//	if err != nil {
+//		log.Errorf("err:%v", err)
+//	}
+//
+//	tmpl, _ := template.New(f.ModelName).Parse(f.Template)
+//	if err != nil {
+//		log.Errorf("err is %v", err)
+//		return
+//	}
+//	err = tmpl.Execute(file, f)
+//	if err != nil {
+//		log.Errorf("err is %v", err)
+//		return
+//	}
+//	return
+// }
+//
+// func GetOsFile(filePath string, fileName string) (file *os.File, err error) {
+//	_, err = os.Stat(filePath)
+//	if os.IsNotExist(err) {
+//		err = os.MkdirAll(filePath, os.ModePerm)
+//		if err != nil {
+//			log.Errorf("err:%v", err)
+//			return
+//		}
+//	}
+//	file, err = os.Create(fmt.Sprintf("%s/%s.go", filePath, fileName))
+//	return
+// }
