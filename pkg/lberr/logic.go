5d03e2c7424bc1c801f0e371e07cc948e49184e3
baigege <1005777562@qq.com>
2022-10-15 11:00:06 +0800

    feat: CreateLbErr

--- pkg/lberr/logic.go
+++ pkg/lberr/logic.go
@@ -1,10 +1,12 @@
 package lberr
 
-var errList []error
+import "fmt"
+
+var errMap = make(map[uint32]*LbErr)
 
 func Register(err ...*LbErr) {
 	for _, lbErr := range err {
-		errList = append(errList, lbErr)
+		errMap[lbErr.code] = lbErr
 	}
 }
 
@@ -16,4 +18,12 @@ func GetErrCode(err error) uint32 {
 func GetErrMessage(err error) string {
 	lbErr := err.(*LbErr)
 	return lbErr.Message()
+}
+
+func CreateLbErr(code uint32) *LbErr {
+	lbErr, ok := errMap[code]
+	if ok {
+		return lbErr
+	}
+	return &LbErr{code: code, message: fmt.Sprintf("unknown code %d", code)}
 }
