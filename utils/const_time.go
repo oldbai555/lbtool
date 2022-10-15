1af158537bc9ba298795548d5dd5b74a25f9626d
baigege <1005777562@qq.com>
2022-10-15 11:14:04 +0800

    feat: rename

---
+++ utils/const_time.go
@@ -0,0 +1,15 @@
+package utils
+
+const (
+	TimeLayout     = "15:04:05"
+	DateLayout     = "2006-01-02"
+	DateTimeLayout = "2006-01-02 15:04:05"
+	YYmmDDLayout   = "20060102"
+)
+
+const (
+	Seconds = 1
+	Minutes = 60 * Seconds
+	Hours   = 60 * Minutes
+	Days    = 24 * Hours
+)
--- utils/time.go
+++ utils/time.go
@@ -10,20 +10,6 @@ var BeiJinTime = time.FixedZone("Beijing Time", int((8 * time.Hour).Seconds()))
 
 var PRCLocation = BeiJinTime
 
-const (
-	TimeLayout     = "15:04:05"
-	DateLayout     = "2006-01-02"
-	DateTimeLayout = "2006-01-02 15:04:05"
-	YYmmDDLayout   = "20060102"
-)
-
-const (
-	Seconds = 1
-	Minutes = 60 * Seconds
-	Hours   = 60 * Minutes
-	Days    = 24 * Hours
-)
-
 func init() {
 	SetupTimezone()
 }
