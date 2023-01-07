9333ad32fe733cef6b341404bf2a9236a92ef891
zhangjianjun <1005777562@qq.com>
2023-01-08 02:22:26 +0800

    feat: FileExists

--- utils/file.go
+++ utils/file.go
@@ -80,3 +80,12 @@ func getCurrentAbPathByCaller() string {
 	}
 	return abPath
 }
+
+// FileExists 文件是否存在
+func FileExists(name string) bool {
+	_, err := os.Stat(name)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
+}
