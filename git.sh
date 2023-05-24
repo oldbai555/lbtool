113e64cdfb466645f9104b3496421910e138c0c9
zhangjianjun <1005777562@qq.com>
2023-05-24 19:53:08 +0800

    v0.0.1

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-git reset "$(git merge-base 176d214b "$(git branch --show-current)")"
-git add -A && git commit -m 'ok'
+git reset "$(git merge-base master "$(git branch --show-current)")"
+git add -A && git commit -m 'v0.0.1'
  git push --force
 # git remote set-url origin https://<你的令牌>@github.com/<你的git用户名>/<要修改的仓库名>.git
